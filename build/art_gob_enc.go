6eeec09d57f649ccf42a1ab578fca64dd321ad80
Nelson Li <nelsonli@google.com>
2025-08-06 03:49:02 +0000

    [ART] Add a Soong module to collect art host test data
    This change introduces a new Soong module type,
    art_host_test_data, designed to replicate the functionality of
    the legacy art_common Make module in a Soong-only build environment.
    
    The primary goal is to address b/431634358 by converting the
    art-host-tests zip package from Make to a Soong test_suite_package.
    A major blocker was the art_common module, which uses a complex,
    Make-based mechanism to collect essential host-side testing files
    (binaries, libraries, boot images). This mechanism fails in a
    SOONG_ONLY=true build.
    
    This implementation used a sandbox-friendly zip packaging mechanism:
    
    1. Singleton for Data Collection & Packaging: The companion singleton
       (art_host_test_data_singleton) still traverses all modules to
       find those providing testInstallInfoProvider. However, it now
       directly generates a build rule to package all collected host files
       into a single art_host_test_data.zip archive using the
       soong_zip tool. This approach ensures all source files are declared
       as explicit dependencies.
    
    2. Conflict Resolution: The singleton includes logic to deduplicate
       file entries that map to the same destination path. It prioritizes
       source files from out/soong/.intermediates directories, as these are
       the direct and most reliable outputs from Soong modules.
    
    3. Proxy Module: The art_host_test_data module now acts as a
       simple proxy or handle for the zip file generated by the singleton.
       Other modules, such as the test_suite_package, can depend on it to
       consume the packaged data, which will later be merged into the final
       test suite zip.
    
    Flag: EXEMPT PURE_REFACTOR
    
    Bug: 431634358
    Test: m -j art-host-tests-bp
    Change-Id: I41d4b422be68247cfa41c7016d17ca2739baeb5d

--- build/art_gob_enc.go
+++ build/art_gob_enc.go
@@ -3,6 +3,7 @@
 package art
 
 import (
+	"android/soong/android"
 	"bytes"
 	"github.com/google/blueprint/gobtools"
 )
@@ -26,7 +27,7 @@ func (r testInstallInfo) Encode(ctx gobtools.EncContext, buf *bytes.Buffer) erro
 			if err = gobtools.EncodeString(buf, k); err != nil {
 				return err
 			}
-			if err = gobtools.EncodeString(buf, v); err != nil {
+			if err = gobtools.EncodeInterface(ctx, buf, v); err != nil {
 				return err
 			}
 		}
@@ -72,45 +73,48 @@ func (r *testInstallInfo) Decode(ctx gobtools.EncContext, buf *bytes.Reader) err
 		return err
 	}
 	if val1 != -1 {
-		r.Testcases = make(map[string]string, val1)
+		r.Testcases = make(map[string]android.Path, val1)
 		for val2 := 0; val2 < int(val1); val2++ {
 			var k string
-			var v string
+			var v android.Path
 			err = gobtools.DecodeString(buf, &k)
 			if err != nil {
 				return err
 			}
-			err = gobtools.DecodeString(buf, &v)
-			if err != nil {
+			if val5, err := gobtools.DecodeInterface(ctx, buf); err != nil {
 				return err
+			} else if val5 == nil {
+				v = nil
+			} else {
+				v = val5.(android.Path)
 			}
 			r.Testcases[k] = v
 		}
 	}
 
-	var val5 int32
-	err = gobtools.DecodeSimple[int32](buf, &val5)
+	var val6 int32
+	err = gobtools.DecodeSimple[int32](buf, &val6)
 	if err != nil {
 		return err
 	}
-	if val5 != -1 {
-		r.TestMap = make(map[string][]string, val5)
-		for val6 := 0; val6 < int(val5); val6++ {
+	if val6 != -1 {
+		r.TestMap = make(map[string][]string, val6)
+		for val7 := 0; val7 < int(val6); val7++ {
 			var k string
 			var v []string
 			err = gobtools.DecodeString(buf, &k)
 			if err != nil {
 				return err
 			}
-			var val9 int32
-			err = gobtools.DecodeSimple[int32](buf, &val9)
+			var val10 int32
+			err = gobtools.DecodeSimple[int32](buf, &val10)
 			if err != nil {
 				return err
 			}
-			if val9 != -1 {
-				v = make([]string, val9)
-				for val10 := 0; val10 < int(val9); val10++ {
-					err = gobtools.DecodeString(buf, &v[val10])
+			if val10 != -1 {
+				v = make([]string, val10)
+				for val11 := 0; val11 < int(val10); val11++ {
+					err = gobtools.DecodeString(buf, &v[val11])
 					if err != nil {
 						return err
 					}
