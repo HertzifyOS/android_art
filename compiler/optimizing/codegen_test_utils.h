9aeafac69b08ec8fc8333bbbf8d067e67c35ffd2
Chris Jones <christopher.jones@arm.com>
2025-09-24 08:49:24 -0700

    [Sim] Setup custom simulator
    Setup a custom ART simulator that can run Arm64 tests on an x86_64
    host machine. This patch does not support the actual running of the
    simulator but sets up the necessary components to enable this in
    future patches. Namely this patch does the following:
    
    - Refactor CodeSimulator and CodeSimulatorContainer classes to
      separate the semantics of basic and runtime-aware simulation.
    - Intercept branches to C++ entrypoints for the custom simulator.
      C++ entrypoint functions have a native context (are compiled as
      x86_64) and therefore there needs to be a transition in ISA/ABI
      when calling these functions from a quick context, i.e:
      entrypoints written in Arm64 assembly. To perform this transition,
      VIXL "runtime calls" will be used via the branch interception
      interface provided by VIXL.
    - Add a simulator version of the generic JNI trampoline to be used
      to handle the transition between native and quick code.
    
    Original change: https://android-review.googlesource.com/c/platform/art/+/2680027/12
    
    Authors: Artem Serov <artem.serov@linaro.org>
             Greg Cawthorne <greg.cawthorne@linaro.org>
             Chris Jones <christopher.jones@arm.com>
    
    Test: test.py --host --target
    Flag: build.ART_USE_SIMULATOR
    Cherrypick-From: https://partner-android-review.googlesource.com/q/commit:d8fa278219313eab1561cf9a962ced1ac4cdee9d
    Merged-In: I7920cd376f9822fb866ecc6337b6cf4ff69c4cc2
    Change-Id: I7920cd376f9822fb866ecc6337b6cf4ff69c4cc2

--- compiler/optimizing/codegen_test_utils.h
+++ compiler/optimizing/codegen_test_utils.h
@@ -174,33 +174,34 @@ static bool CanExecuteOnHardware(const CodeGenerator& codegen) {
       && InstructionSetFeatures::FromHwcap()->HasAtLeast(isa_features);
 }
 
+static constexpr size_t kDefaultStackSize = 1 * MB;
+
 static bool CanExecuteISA(InstructionSet target_isa) {
-  std::unique_ptr<CodeSimulator> simulator(CreateCodeSimulator(target_isa));
-  return DoesHardwareSupportISA(target_isa) || bool(simulator);
+  return DoesHardwareSupportISA(target_isa) || BasicCodeSimulator::CanSimulate(target_isa);
 }
 
 static bool CanExecute(const CodeGenerator& codegen) {
-  std::unique_ptr<CodeSimulator> simulator(CreateCodeSimulator(codegen.GetInstructionSet()));
-  return CanExecuteOnHardware(codegen) || bool(simulator);
+  return CanExecuteOnHardware(codegen) ||
+         BasicCodeSimulator::CanSimulate(codegen.GetInstructionSet());
 }
 
 template <typename Expected>
-inline static Expected SimulatorExecute(CodeSimulator* simulator, Expected (*f)());
+inline static Expected SimulatorExecute(BasicCodeSimulator* simulator, Expected (*f)());
 
 template <>
-inline bool SimulatorExecute<bool>(CodeSimulator* simulator, bool (*f)()) {
+inline bool SimulatorExecute<bool>(BasicCodeSimulator* simulator, bool (*f)()) {
   simulator->RunFrom(reinterpret_cast<intptr_t>(f));
   return simulator->GetCReturnBool();
 }
 
 template <>
-inline int32_t SimulatorExecute<int32_t>(CodeSimulator* simulator, int32_t (*f)()) {
+inline int32_t SimulatorExecute<int32_t>(BasicCodeSimulator* simulator, int32_t (*f)()) {
   simulator->RunFrom(reinterpret_cast<intptr_t>(f));
   return simulator->GetCReturnInt32();
 }
 
 template <>
-inline int64_t SimulatorExecute<int64_t>(CodeSimulator* simulator, int64_t (*f)()) {
+inline int64_t SimulatorExecute<int64_t>(BasicCodeSimulator* simulator, int64_t (*f)()) {
   simulator->RunFrom(reinterpret_cast<intptr_t>(f));
   return simulator->GetCReturnInt64();
 }
@@ -213,9 +214,14 @@ static void VerifyGeneratedCode(const CodeGenerator& codegen,
   ASSERT_TRUE(CanExecute(codegen)) << "Target isa is not executable.";
 
   // Verify on simulator.
-  std::unique_ptr<CodeSimulator> simulator(CreateCodeSimulator(codegen.GetInstructionSet()));
-  if (simulator) {
+  InstructionSet target_isa = codegen.GetInstructionSet();
+  if (BasicCodeSimulator::CanSimulate(target_isa)) {
+    // Use basic simulator: for the gtests we don't have runtime started, so won't have entrypoints
+    // initialized.
+    std::unique_ptr<BasicCodeSimulator> simulator(
+        CreateBasicCodeSimulator(codegen.GetInstructionSet(), kDefaultStackSize));
     Expected result = SimulatorExecute<Expected>(simulator.get(), f);
+
     if (has_result) {
       ASSERT_EQ(expected, result);
     }
