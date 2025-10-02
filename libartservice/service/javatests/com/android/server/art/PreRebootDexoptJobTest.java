HertzifyOS/android_art

commit 9c87a27ddd24250c13290eecd3ad0bd64688693f

Handle the cases where the original update was superseded or revoked.
In these cases, UpdateEngine.triggerPostinstall throws exceptions.

Before this change, we treat them as failures, logging wtf and reporting
STATUS_FAILED.

After this change, we treat them as cancellations, reporting
STATUS_FINISHED (processed as STATUS_CANCELLED by the server) and not
logging wtf.

Bug: 427701203
Test: Presubmit
Flag: EXEMPT BUGFIX
Change-Id: Ifa388e21d87c0c996d65b0636f2de56568935381

libartservice/service/java/com/android/server/art/PreRebootDexoptJob.java
@@ -452,6 +452,24 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
         try {
             mInjector.getUpdateEngine().triggerPostinstall("system" /* partition */);
         } catch (ServiceSpecificException e) {
+            // From system/update_engine/common/error_code.h.
+            final int POSTINTALL_RUNNER_ERROR = 5;
+            final int UPDATE_PROCESSING = 65;
+            // update_engine threw "Already processing an update, cancel it first."
+            // It probably means a newer update is superseding the original update.
+            boolean updateSuperseded = e.errorCode == UPDATE_PROCESSING;
+            // update_engine threw "Postinstall action did not run. OTA update must first reach the
+            // Postinstall phase(which verfies that all partitions can be mounted) before calling
+            // TriggerPostinstall".
+            // It probably means the original update has been revoked.
+            boolean updateRevoked =
+                    e.errorCode == POSTINTALL_RUNNER_ERROR
+                            && e.getMessage().contains("TriggerPostinstall");
+            if (updateSuperseded || updateRevoked) {
+                AsLog.i("update_engine error ignored: " + e.getMessage());
+                cancelAny();
+                return;
+            }
             throw new UpdateEngineException("Failed to trigger postinstall: " + e.getMessage());
         }
         long startTime = System.currentTimeMillis();

commit e40cd55605eecb587bac946ba3af7b6388426475

Fix deadlock issue in Pre-reboot Dexopt.
commit 9c87a27ddd24250c13290eecd3ad0bd64688693f introduced a deadlock
issue in Pre-reboot Dexopt. The issue is triggered when an OTA update is
revoked or superseded.

This CL fixes it.

Bug: 448068940
Test: atest ArtServiceTests
Test: -
  1. Install an OTA, but don't reboot.
  2. Revoke the OTA (adb shell update_engine_client --reset_status)
  3. Trigger Pre-reboot Dexopt (adb shell cmd jobscheduler run android 27873781)
  4. Install an OTA again.
  5. Before this change, step 4 will be stuck in the postinstall phase.
     After this change, it will no longer be stuck.
Flag: EXEMPT BUGFIX

Change-Id: Ie65d50584576115cdcf9428cc12734edeeab48ba

libartservice/service/java/com/android/server/art/PreRebootDexoptJob.java
@@ -471,7 +471,9 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
                             && e.getMessage().contains("TriggerPostinstall");
             if (updateSuperseded || updateRevoked) {
                 AsLog.i("update_engine error ignored: " + e.getMessage());
-                cancelAny();
+                synchronized (this) {
+                    mCancellationSignal.cancel();
+                }
                 return;
             }
             throw new UpdateEngineException("Failed to trigger postinstall: " + e.getMessage());

libartservice/service/javatests/com/android/server/art/PreRebootDexoptJobTest.java
@@ -24,6 +24,7 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyBoolean;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.isNull;
 import static org.mockito.Mockito.lenient;
@@ -305,6 +306,43 @@ public class PreRebootDexoptJobTest {
     public void testStartMainline() throws Exception {
         checkStart(null /* otaSlot */, () -> anyBoolean() /* mapSnapshotsForOtaMatcher */);
         verify(mUpdateEngine, never()).triggerPostinstall(any());
+    }
+
+    @Test
+    @EnableFlags({android.os.Flags.FLAG_UPDATE_ENGINE_API})
+    public void testStartWithUpdateEngineApiSkippedDueToUpdateGone() throws Exception {
+        final int POSTINTALL_RUNNER_ERROR = 5;
+        doThrow(new ServiceSpecificException(POSTINTALL_RUNNER_ERROR,
+                        "Postinstall action did not run. OTA update must first reach the "
+                                + "Postinstall phase(which verfies that all partitions can be "
+                                + "mounted) before calling TriggerPostinstall"))
+                .when(mUpdateEngine)
+                .triggerPostinstall("system");
+
+        mPreRebootDexoptJob.onUpdateReadyImpl("_b");
+        mPreRebootDexoptJob.onStartJobImpl(mJobService, mJobParameters);
+
+        mPreRebootDexoptJob.waitForRunningJob();
+
+        mPreRebootStatsReporterHarness.recordFakeAfterRebootDataAndReport();
+        mPreRebootStatsReporterHarness.verifyJobStats(Status.STATUS_FINISHED);
+    }
+
+    @Test
+    @EnableFlags({android.os.Flags.FLAG_UPDATE_ENGINE_API})
+    public void testStartWithUpdateEngineApiFailedDueToUnknownError() throws Exception {
+        final int POSTINTALL_RUNNER_ERROR = 5;
+        doThrow(new ServiceSpecificException(POSTINTALL_RUNNER_ERROR, "Some unknown error"))
+                .when(mUpdateEngine)
+                .triggerPostinstall("system");
+
+        mPreRebootDexoptJob.onUpdateReadyImpl("_b");
+        mPreRebootDexoptJob.onStartJobImpl(mJobService, mJobParameters);
+
+        mPreRebootDexoptJob.waitForRunningJob();
+
+        mPreRebootStatsReporterHarness.recordFakeAfterRebootDataAndReport();
+        mPreRebootStatsReporterHarness.verifyJobStats(Status.STATUS_FAILED);
     }
 
     private void checkSyncStart(boolean isUpdateEngineReady, boolean expectedMapSnapshotsForOta)