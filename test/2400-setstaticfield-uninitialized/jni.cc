2a5fde17af04468d556b92a49dee8c3b5c2dda9f
Vladimir Marko <vmarko@google.com>
2025-12-04 07:29:05 +0000

    ART: Fix lint issues in run-test 2400.
    Test: tools/repohooks/tools/cpplint.py --quiet \
          art/test/2400-setstaticfield-uninitialized/jni.cc
    Bug: 465779655
    Flag: EXEMPT BUGFIX
    Change-Id: I3c0892ee3534f5ac725959a7c345b61a718b114d

--- test/2400-setstaticfield-uninitialized/jni.cc
+++ test/2400-setstaticfield-uninitialized/jni.cc
@@ -24,62 +24,62 @@ extern "C" JNIEXPORT void JNICALL Java_Main_setBooleanFieldJni(
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "Z");
   env->SetStaticBooleanField(clazz, field, new_value);
-};
+}
 
 extern "C" JNIEXPORT void JNICALL Java_Main_setByteFieldJni(
     JNIEnv* env, [[maybe_unused]] jclass, jclass clazz, jstring jfield_name, jbyte new_value) {
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "B");
   env->SetStaticByteField(clazz, field, new_value);
-};
+}
 
 extern "C" JNIEXPORT void JNICALL Java_Main_setCharFieldJni(
     JNIEnv* env, [[maybe_unused]] jclass, jclass clazz, jstring jfield_name, jchar new_value) {
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "C");
   env->SetStaticCharField(clazz, field, new_value);
-};
+}
 
 extern "C" JNIEXPORT void JNICALL Java_Main_setShortFieldJni(
     JNIEnv* env, [[maybe_unused]] jclass, jclass clazz, jstring jfield_name, jshort new_value) {
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "S");
   env->SetStaticShortField(clazz, field, new_value);
-};
+}
 
 extern "C" JNIEXPORT void JNICALL Java_Main_setIntFieldJni(
     JNIEnv* env, [[maybe_unused]] jclass, jclass clazz, jstring jfield_name, jint new_value) {
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "I");
   env->SetStaticIntField(clazz, field, new_value);
-};
+}
 
 extern "C" JNIEXPORT void JNICALL Java_Main_setFloatFieldJni(
     JNIEnv* env, [[maybe_unused]] jclass, jclass clazz, jstring jfield_name, jfloat new_value) {
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "F");
   env->SetStaticFloatField(clazz, field, new_value);
-};
+}
 
 extern "C" JNIEXPORT void JNICALL Java_Main_setLongFieldJni(
     JNIEnv* env, [[maybe_unused]] jclass, jclass clazz, jstring jfield_name, jlong new_value) {
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "J");
   env->SetStaticLongField(clazz, field, new_value);
-};
+}
 
 extern "C" JNIEXPORT void JNICALL Java_Main_setDoubleFieldJni(
     JNIEnv* env, [[maybe_unused]] jclass, jclass clazz, jstring jfield_name, jdouble new_value) {
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "D");
   env->SetStaticDoubleField(clazz, field, new_value);
-};
+}
 
 extern "C" JNIEXPORT void JNICALL Java_Main_setRefFieldJni(
     JNIEnv* env, [[maybe_unused]] jclass, jclass clazz, jstring jfield_name, jobject new_value) {
   ScopedUtfChars field_name(env, jfield_name);
   jfieldID field = env->GetStaticFieldID(clazz, field_name.c_str(), "Ljava/lang/Object;");
   env->SetStaticObjectField(clazz, field, new_value);
-};
-
 }
+
+}  // namespace art
