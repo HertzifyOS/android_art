69b86169a4b7b4d5cbf214de891d902fae2d0b8f
Jared Duke <jdduke@google.com>
2025-12-06 01:20:18 +0000

    Require explicit feature set for Arm64Assembler
    Require an explicit InstructionSetFeatures arg when constructing a new
    Arm64Assembler instance. While this is allowed to be null (for now), it
    at least ensures the callsite is explicit about leaving out the current
    or target set of instruction set features.
    
    Bug: 461125269
    Test: presubmit
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: I91ac70ca3d7363e9c9afd3d435e2ab741e7d103d

--- dex2oat/driver/compiler_driver.cc
+++ dex2oat/driver/compiler_driver.cc
@@ -280,30 +280,35 @@ CompilerDriver::~CompilerDriver() {
       });
 }
 
+#define CREATE_TRAMPOLINE_IMPL(isa, isa_features, type, abi, offset)                     \
+  if (Is64BitInstructionSet((isa))) {                                                    \
+    return CreateTrampoline64(                                                           \
+        (isa), (isa_features), abi, type##_ENTRYPOINT_OFFSET(PointerSize::k64, offset)); \
+  } else {                                                                               \
+    return CreateTrampoline32(                                                           \
+        (isa), (isa_features), abi, type##_ENTRYPOINT_OFFSET(PointerSize::k32, offset)); \
+  }
 
-#define CREATE_TRAMPOLINE_IMPL(isa, type, abi, offset)                                  \
-    if (Is64BitInstructionSet((isa))) {                                                 \
-      return CreateTrampoline64((isa),                                                  \
-                                abi,                                                    \
-                                type ## _ENTRYPOINT_OFFSET(PointerSize::k64, offset));  \
-    } else {                                                                            \
-      return CreateTrampoline32((isa),                                                  \
-                                abi,                                                    \
-                                type ## _ENTRYPOINT_OFFSET(PointerSize::k32, offset));  \
-    }
-
-#define CREATE_TRAMPOLINE(type, abi, offset)                                            \
-    CREATE_TRAMPOLINE_IMPL(GetCompilerOptions().GetInstructionSet(), type, abi, offset)
+#define CREATE_TRAMPOLINE(type, abi, offset)                               \
+  CREATE_TRAMPOLINE_IMPL(GetCompilerOptions().GetInstructionSet(),         \
+                         GetCompilerOptions().GetInstructionSetFeatures(), \
+                         type,                                             \
+                         abi,                                              \
+                         offset)
 
 std::unique_ptr<const std::vector<uint8_t>>
     CompilerDriver::CreateJniDlsymLookupTrampoline() const {
 #ifdef ART_USE_SIMULATOR
   // In simulator mode the dlsymLookup trampoline is executed in native ISA, not quick code isa.
   InstructionSet trampoline_isa = kRuntimeISA;
+  // Don't assume any specialized ISA features.
+  const InstructionSetFeatures* trampoline_isa_features = nullptr;
 #else
   InstructionSet trampoline_isa = GetCompilerOptions().GetInstructionSet();
+  const InstructionSetFeatures* trampoline_isa_features =
+      GetCompilerOptions().GetInstructionSetFeatures();
 #endif
-  CREATE_TRAMPOLINE_IMPL(trampoline_isa, JNI, kJniAbi, pDlsymLookup)
+  CREATE_TRAMPOLINE_IMPL(trampoline_isa, trampoline_isa_features, JNI, kJniAbi, pDlsymLookup)
 }
 
 std::unique_ptr<const std::vector<uint8_t>>
