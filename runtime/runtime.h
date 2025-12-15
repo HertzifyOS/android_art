cb393f9e968925ad98b54d5a402a31c7c5e732bf
Jared Duke <jdduke@google.com>
2025-12-15 22:14:12 +0000

    arm64: Augment quick lock entrypoints with LSE atomics
    When LSE atomics are available, use CAS instructions for the quick lock
    entrypoints instead of the exclusive load/store alternative. This should
    be a performance win for contested scenarios. It also seamlessly handles
    the read barrier (enabled/disabled) case when unlocking.
    
    Bug: 474165956
    Test: presubmit
    Flag: com.android.art.rw.flags.lse_quick_lock
    Change-Id: I6be71ea24175e3a00452167989e7ff201c857354

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -107,6 +107,7 @@ class CompilerCallbacks;
 class Dex2oatImageTest;
 class DexFile;
 enum class InstructionSet;
+class InstructionSetFeatures;
 class InternTable;
 class IsMarkedVisitor;
 class JavaVMExt;
@@ -1013,6 +1014,10 @@ class Runtime {
   // suspended to call this function.
   EXPORT void SetJniIdType(JniIdType t);
 
+  const InstructionSetFeatures* GetRuntimeInstructionSetFeatures() const {
+    return runtime_instruction_set_features_.get();
+  }
+
   uint32_t GetVerifierLoggingThresholdMs() const {
     return verifier_logging_threshold_ms_;
   }
@@ -1412,6 +1417,8 @@ class Runtime {
 
   // ART counterpart for the compat framework (go/compat-framework).
   CompatFramework compat_framework_;
+
+  std::unique_ptr<const InstructionSetFeatures> runtime_instruction_set_features_;
 
   // Implicit checks flags.
   bool implicit_null_checks_;       // NullPointer checks are implicit.
