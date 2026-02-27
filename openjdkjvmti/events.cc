6d8d1ea354a65685aac3060e040f4ead04fc39ad
Nicolas Geoffray <ngeoffray@google.com>
2026-02-27 11:59:17 +0000

    Two fixes following 3f8f62cd4a85f16984964c81cd96417ab7922dd5.
    Lock level shouldn't have changed, and fix line width.
    
    Test: test.py
    Flag: EXEMPT PURE_REFACTOR
    Bug: 481456995
    Change-Id: I230c9127a040bf6302f6bcaf5fe3df2318a51d71

--- openjdkjvmti/events.cc
+++ openjdkjvmti/events.cc
@@ -1651,7 +1651,7 @@ void EventHandler::Shutdown() {
 }
 
 EventHandler::EventHandler()
-  : envs_lock_("JVMTI Environment List Lock", art::LockLevel::kAllocTrackerLock),
+  : envs_lock_("JVMTI Environment List Lock", art::LockLevel::kPostMutatorTopLockLevel),
     frame_pop_enabled(false),
     internal_event_refcount_({0}) {
   alloc_listener_.reset(new JvmtiEventAllocationListener(this));
